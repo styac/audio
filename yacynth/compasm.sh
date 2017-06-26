80c7e65c2d3c2dffdbf8c874031677196106027f
si <ist@linux-9s1a.suse>
2017-06-26 19:34:16 +0200

    fix cmake

--- yacynth/compasm.sh
+++
@@ -1,3 +0,0 @@
-
-g++-6 -S -O3 -mfpmath=sse -msse2 -msse3 -msse4.2 -ffast-math -funsafe-math-optimizations -mfma -mfma4 -mavx -funroll-loops -funsafe-loop-optimizations -funswitch-loops -fvariable-expansion-in-unroller -ftree-vectorizer-verbose=2   -c -O2 -Isrc/include -std=c++14 -MMD -MP -MF "build/Release/GNU-Linux/src/effects/Filter.o.d" -o build/Release/GNU-Linux/src/effects/Filter.s src/effects/Filter.cpp
-
---
+++ yacynth/run_cmake.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+export SRCDIR=`pwd`
+export BINDIR=${SRCDIR}-bin
+
+mkdir ${BINDIR}
+
+if [ -d "$BINDIR" ]; then
+	cd ${BINDIR}
+	ccmake ${SRCDIR}
+	echo "***  cd ${BINDIR} and run make **** "
+else 
+	echo "***  can't make ${BINDIR} check access rights ***"
+fi
