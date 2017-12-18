f9de871600816b69871c354823ca8bf67f96ed3e
SI <stevens37@gmail.com>
2017-12-18 16:25:36 +0100

    cleanup

--- yacynth/run_cmake.sh
+++ yacynth/run_cmake.sh
@@ -1,13 +1,22 @@
 #/bin/bash
 
 export SRCDIR=`pwd`
-export BINDIR=${SRCDIR}_bin
+export BINDIR=${SRCDIR}_build
+export GENERATOR=" "
+
+if [ "${1,,}" == "ninja" ]
+then 
+	GENERATOR="-G Ninja "
+	echo " ** generate Ninja files"
+else
+	echo " ** generate make files"
+fi
 
 mkdir ${BINDIR}
 
 if [ -d "$BINDIR" ]; then
 	cd ${BINDIR}
-	ccmake -D CMAKE_C_COMPILER=/usr/bin/gcc-6 -D CMAKE_CXX_COMPILER=/usr/bin/g++-6   ${SRCDIR}
+	cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-6 -D CMAKE_CXX_COMPILER=/usr/bin/g++-6 ${GENERATOR}  ${SRCDIR}
 	echo "***  cd ${BINDIR} and run make **** "
 else 
 	echo "***  can't make ${BINDIR} check access rights ***"
